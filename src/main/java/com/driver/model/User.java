31273b7847d6b748fb3da39681f8a7fb157450c7
parveenjoon <47267545+parveenjoon@users.noreply.github.com>
2024-04-04 19:12:51 +0530

    Update User.java

--- src/main/java/com/driver/model/User.java
+++ src/main/java/com/driver/model/User.java
@@ -1,3 +1,4 @@
+
 package com.driver.model;
 
 import javax.persistence.*;
@@ -5,35 +6,22 @@ import java.util.List;
 
 @Entity
 public class User {
-
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private int id;
+    private Integer id;
+    String name;
+    String phoneNumber;
+    String password;
 
     @OneToMany(mappedBy = "user", cascade = CascadeType.ALL)
-    private List<Reservation> reservationList;
+    List<Reservation> reservationList;
 
-    private String name;
-    private String phoneNumber;
-    private String password;
-
-    public User() {}
-
-    // Getters and setters
-    public int getId() {
+    public Integer getId() {
         return id;
     }
 
-    public void setId(int id) {
+    public void setId(Integer id) {
         this.id = id;
-    }
-
-    public List<Reservation> getReservationList() {
-        return reservationList;
-    }
-
-    public void setReservationList(List<Reservation> reservationList) {
-        this.reservationList = reservationList;
     }
 
     public String getName() {
@@ -58,5 +46,13 @@ public class User {
 
     public void setPassword(String password) {
         this.password = password;
+    }
+
+    public List<Reservation> getReservationList() {
+        return reservationList;
+    }
+
+    public void setReservationList(List<Reservation> reservationList) {
+        this.reservationList = reservationList;
     }
 }
