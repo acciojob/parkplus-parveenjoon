084ea1f2b7d568716e33c9bfcfd808f55319faef
parveenjoon <47267545+parveenjoon@users.noreply.github.com>
2024-04-04 19:16:52 +0530

    Update ParkingLotService.java

--- src/main/java/com/driver/services/ParkingLotService.java
+++ src/main/java/com/driver/services/ParkingLotService.java
@@ -1,3 +1,4 @@
+
 package com.driver.services;
 
 import com.driver.model.ParkingLot;
@@ -5,13 +6,13 @@ import com.driver.model.Spot;
 
 public interface ParkingLotService {
 
-    ParkingLot addParkingLot(String name, String address);
-
-    Spot addSpot(int parkingLotId, Integer numberOfWheels, Integer pricePerHour);
-
     void deleteSpot(int spotId);
 
     Spot updateSpot(int parkingLotId, int spotId, int pricePerHour);
 
     void deleteParkingLot(int parkingLotId);
+
+    ParkingLot addParkingLot(String name, String address);
+
+    Spot addSpot(int parkingLotId, Integer numberOfWheels, Integer pricePerHour);
 }
