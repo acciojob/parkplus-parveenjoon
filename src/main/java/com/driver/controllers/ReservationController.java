3ebb4e8acd4c704542a58fdeb07eac2af08db479
parveenjoon <47267545+parveenjoon@users.noreply.github.com>
2024-04-04 19:10:40 +0530

    Update ReservationController.java

--- src/main/java/com/driver/controllers/ReservationController.java
+++ src/main/java/com/driver/controllers/ReservationController.java
@@ -1,5 +1,7 @@
+
 package com.driver.controllers;
 
+import com.driver.model.Reservation;
 import com.driver.services.impl.ReservationServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -13,7 +15,10 @@ public class ReservationController {
     @Autowired
     ReservationServiceImpl reservationService;
     @PostMapping("/reserveSpot")
-    public Reservation reserveSpot(@RequestParam Integer userId, @RequestParam Integer parkingLotId, @RequestParam Integer timeInHours, @RequestParam Integer numberOfWheels) throws Exception {
+    public Reservation reserveSpot(@RequestParam Integer userId, @RequestParam Integer parkingLotId, @RequestParam Integer timeInHours, @RequestParam Integer numberOfWheels) throws Exception{
+        //Reserve a spot in the given parkingLot such that the total price is minimum. Note that the price per hour for each spot is different
+        //Note that the vehicle can only be parked in a spot having a type equal to or larger than given vehicle
+        //If parkingLot is not found, user is not found, or no spot is available, throw "Cannot make reservation" exception.
         return reservationService.reserveSpot(userId, parkingLotId, timeInHours, numberOfWheels);
     }
 }
