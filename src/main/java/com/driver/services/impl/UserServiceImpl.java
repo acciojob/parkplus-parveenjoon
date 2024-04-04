08b65694883552ac01bb380f9d4e48a6eb0c382c
parveenjoon <47267545+parveenjoon@users.noreply.github.com>
2024-04-04 19:15:49 +0530

    Update UserServiceImpl.java

--- src/main/java/com/driver/services/impl/UserServiceImpl.java
+++ src/main/java/com/driver/services/impl/UserServiceImpl.java
@@ -1,11 +1,14 @@
+
 package com.driver.services.impl;
 
+import com.driver.model.User;
 import com.driver.repository.UserRepository;
 import com.driver.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
+import java.util.Optional;
 
 @Service
 public class UserServiceImpl implements UserService {
@@ -14,23 +17,24 @@ public class UserServiceImpl implements UserService {
     UserRepository userRepository4;
     @Override
     public void deleteUser(Integer userId) {
-        userRepository.deleteById(userId);
+        userRepository4.deleteById(userId);
     }
 
     @Override
     public User updatePassword(Integer userId, String password) {
-        User user = userRepository.findById(userId)
-                .orElseThrow(() -> new RuntimeException("User not found"));
-        user.setPassword(password);
-        return userRepository.save(user);
+        Optional<User> optUser = userRepository4.findById(userId);
+        User userObj = optUser.get();
+        userObj.setPassword(password);
+        User user = userRepository4.save(userObj);
+        return user;
     }
 
     @Override
     public void register(String name, String phoneNumber, String password) {
         User user = new User();
         user.setName(name);
-        user.setPhoneNumber(phoneNumber);
         user.setPassword(password);
-        userRepository.save(user);
+        user.setPhoneNumber(phoneNumber);
+        userRepository4.save(user);
     }
 }
